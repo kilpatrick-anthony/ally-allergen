4eb6489fac21339f853787842f5c9460af1c3318
kilpatrick-anthony <kilpatrick.anthony@gmail.com>
2026-04-11 14:13:33 +0000

    fix: remove orphaned closing divs and IIFE closure
    - Remove extra closing divs that were causing JSX mismatch
    - Clean up leftover code structure from refactor

--- app/admin/menu-builder/page.tsx
+++ app/admin/menu-builder/page.tsx
@@ -1124,11 +1124,6 @@ export default function MenuBuilderPage() {
         </div>
       </Card>
 
-            </div>
-          </div>
-        </div>
-      )}
-
       {/* Menu Items Grid */}
       <Card className="mb-8">
         <div className="p-6 border-b dark:border-gray-700">
